7702694492addea0bdd3e8931e31037bcf9ebddd
allantucker <60186352+allantucker@users.noreply.github.com>
2020-01-27 15:35:35 -0500

    Update config.php

--- config.php
+++ config.php
@@ -7,8 +7,8 @@
     // database config
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'awstreams');
+    define('DB_PASSWORD', 'ij6578g89767YGHGbu75');
+    define('DB_NAME', 'homedbftw');
 
     #endregion
 
